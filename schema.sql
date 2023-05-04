1c82f2f711f0a26e4463c5a22ccd9e8112472582
Jaime Villegas <jaimevillegas296@gmail.com>
2023-05-04 11:28:32 -0500

    Work on Join Tables

--- schema.sql
+++ schema.sql
@@ -40,3 +40,29 @@ ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id);
 
 /* Add a column owner_id which is a foreign key referencing owners table */
 ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners(id);
+
+/* add a table vets */
+CREATE TABLE vets(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(300),
+    age INT,
+    date_of_graduation DATE,
+    PRIMARY KEY(id)
+);
+
+/* create a join table called specializations */
+CREATE TABLE specializations(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    vet_id INT REFERENCES vets(id),
+    species_id INT REFERENCES species(id),
+    PRIMARY KEY(id)
+);
+
+/* Create a join table called visits */
+CREATE TABLE visits(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    vet_id INT REFERENCES vets(id),
+    animal_id INT REFERENCES animals(id),
+    visit_date DATE,
+    PRIMARY KEY(id)
+);
